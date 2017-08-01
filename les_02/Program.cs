246ca6532a1f25090a566aac3b97c59e112f3193
Denis Sorokin <denys-s@yit.co.il>
2017-08-01 15:27:56 +0300

    fixed directory

---
+++ les_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace les_02
+{
+    class MyClass
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("myclass");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
